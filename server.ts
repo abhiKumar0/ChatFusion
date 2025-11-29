62abf2da16e19d28f12574d14a3baaf0547cdfa9
Abhishek <ak96166956@gmail.com>
2025-11-30 00:03:44 +0530

    Deploy again

--- server.ts
+++ server.ts
@@ -1,17 +1,19 @@
 import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
-import { initSocket } from "./src/lib/socket-server";
+import { initSocket } from "./src/lib/socket-server.ts";
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const port = process.env.PORT || 3000;
+
 app.prepare().then(() => {
   const server = createServer((req, res) => {
     const parsedUrl = parse(req.url || "/", true);
     handle(req, res, parsedUrl);
-  }).listen(3000, () => {
+  }).listen(port, () => {
     console.log("> Ready on http://localhost:3000");
   });
 
