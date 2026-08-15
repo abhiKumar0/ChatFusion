1f80ff9b8eb3fb4e1ac547f1237f298bb2965ea3
Abhishek Kr <ak96166956@gmail.com>
2026-08-16 01:04:38 +0530

    Bug fixed

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,9 +1,5 @@
 import { Redis } from "@upstash/redis";
 
-
-console.log("URL => ", process.env.UPSTASH_REDIS_REST_URL!);
-console.log("URL => ", process.env.UPSTASH_REDIS_REST_TOKEN!);
-
 const redis = new Redis({
     url: process.env.UPSTASH_REDIS_REST_URL!,
     token: process.env.UPSTASH_REDIS_REST_TOKEN!,
